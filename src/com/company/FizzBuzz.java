a0374426b24dc8cfe60808756590bc77f3b817a9
Daichi0914 <DaichiSugawara44@gmail.com>
2021-07-09 08:38:20 +0900

    initial commit

---
+++ src/com/company/FizzBuzz.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class FizzBuzz {
+    public static String[] fizzBuzz() {
+        String fizz = "...Fizz...";
+        String buzz = "...Buzz...";
+
+        String[] arr;
+        arr = new String[100];
+
+        for (int i = 1; i < 101; i++) {
+            String s = String.valueOf(i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                arr[i - 1] = fizz + buzz;
+            } else if (s.contains("3") && s.contains("5")) {
+                arr[i - 1] = fizz + buzz;
+            } else if (s.contains("3")) {
+                arr[i - 1] = fizz;
+            } else if (s.contains("5")) {
+                arr[i - 1] = buzz;
+            } else if (i % 3 == 0) {
+                arr[i - 1] = fizz;
+            } else if (i % 5 == 0) {
+                arr[i - 1] = buzz;
+            } else {
+                arr[i - 1] = s;
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        for (String el : FizzBuzz.fizzBuzz()) {
+            System.out.println(el);
+        }
+    }
+}
